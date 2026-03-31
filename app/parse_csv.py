cc4c6f0a4e7da8021f225188fef15075f77eb267
James Lei <leijames325@gmail.com>
2026-03-30 20:32:07 -0400

    Parsing CSV changes + devlog

--- app/parse_csv.py
+++ app/parse_csv.py
@@ -3,12 +3,13 @@ from db import insert_query, general_query, select_query
 
 with open('titanic1.csv', newline='') as titanic_data:
     reader = csv.reader(titanic_data)
+    next(reader)
     for row in reader:
         insert_query("DefaultPassengers", {
             "id": row[0],
             "survived": row[1],
             "class": row[2],
-            "sex": row[3],
+            "sex": "female" if row[3] else "male",
             "age": row[4],
             "fare": row[10],
             "isAlone": row[9],
