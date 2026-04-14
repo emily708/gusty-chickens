4f95dff28c0c74b4b6af78c1af396ac345665228
ZWKIR <am226ys@gmail.com>
2026-04-14 11:01:29 -0400

    add map

--- app/game.py
+++ app/game.py
@@ -5,3 +5,18 @@ bp = Blueprint('game', __name__, url_prefix='/game')
 @bp.get('/map')
 def map_get():
     return render_template("map.html")
+
+@bp.get('/rooms/<place>')
+def rooms_get(place):
+    room_names = DefaultRooms().get(name)
+    with sqlite3.connect(DB_FILE) as db:
+        c = db.cursor()
+        # get passengers for the room accessed
+
+        # parse and display on a table in html file
+
+        # add a checkbox next to each passenger to move
+    return access_room(place)
+
+def access_room():
+    return render_template(f"{place}.html")
