1a2c6a00857eaab373ca28b26e93108ed827b0f1
Amy Shrestha <am226ys@gmail.com>
2026-04-16 23:56:53 -0400

    add miscellaneous rooms

--- app/constants.py
+++ app/constants.py
@@ -23,6 +23,45 @@ rooms = {
     }
   },
   "miscellaneous": {
-
-  }
+      "kitchen": {
+          "limit": 5
+      },
+      # limit zero means no limit
+      "library": {
+          "limit": 0
+      },
+      "compass_platform": {
+          "limit": 0
+      },
+      "refrigerated_cargo": {
+          "limit": 0
+      },
+      "first_class": {
+          "limit": 0
+      },
+      "gymnasium": {
+          "limit": 1
+      },
+      "swimming_pool": {
+          "limit": 1
+      },
+      "squash_court": {
+          "limit": 1
+      },
+      "water_tank": {
+          "limit": 0
+      },
+      "boiler_room": {
+          "limit": 0
+      },
+      "post_office": {
+          "limit": 0
+      },
+      "crew_room": {
+          "limit": 0
+      },
+      "cargo": {
+          "limit": 0
+      }
+    }
 }
